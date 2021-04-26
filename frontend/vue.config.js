4c845a106092b3708263b0eccd1c5ab9392025c6
Alexander Strutz <alexander.strutz@able-group.de>
2021-04-26 16:58:49 +0200

    Remove file deletion to use serve

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,9 +3,4 @@ module.exports = {
     'vuetify'
   ],
   filenameHashing: false,
-  chainWebpack: config => {
-    config.plugins.delete('html')
-    config.plugins.delete('preload')
-    config.plugins.delete('prefetch')
-  }
 }
