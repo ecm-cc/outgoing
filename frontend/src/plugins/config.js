22abb2679dcb8345929d43762530b206911d311d
Alexander Strutz <alexander.strutz@able-group.de>
2021-05-17 11:26:05 +0200

    Update configs for qas, version and prod

--- frontend/src/plugins/config.js
+++ frontend/src/plugins/config.js
@@ -9,13 +9,22 @@ const getConfig = async (url) => {
       };
       break;
     case 'able-group-qas.d-velop.cloud': 
-      config = {};
+      config = {
+        host: 'https://able-group-qas.d-velop.cloud',
+        repositoryID: '64bdf712-b328-5f46-8fd0-b8e67aaf8bec',
+      };
       break;
     case 'able-group-version.d-velop.cloud':
-      config = {};
+      config = {
+        host: 'https://able-group-version.d-velop.cloud',
+        repositoryID: '576583f0-8cd0-5796-bc94-e49426e7bbfb',
+      };
       break;
     case 'able-group.d-velop.cloud':
-      config = {};
+      config = {
+        host: 'https://able-group.d-velop.cloud',
+        repositoryID: '1a2cde3f-2913-3dc2-4a2e-e623459ac23a',
+      };
       break;
     }
   config.searchURL = await getSearchURL();
--- global.config.js
+++ global.config.js
@@ -14,12 +14,33 @@ function getLocalConfig(tenant) {
     };
     // QAS
     case '197': return {
+        stage: 'qas',
+        host: 'https://able-group-qas.d-velop.cloud',
+        repositoryID: '64bdf712-b328-5f46-8fd0-b8e67aaf8bec',
+        pageSize: 100,
+        lightsailURL: 'https://ecm-inbound.able-group.de/invoice?stage=qas',
+        lightsailUser: process.env.LIGHTSAIL_USER,
+        lightsailPass: process.env.LIGHTSAIL_PASS,
     };
     // Version
     case '1ha': return {
+        stage: 'version',
+        host: 'https://able-group-version.d-velop.cloud',
+        repositoryID: '576583f0-8cd0-5796-bc94-e49426e7bbfb',
+        pageSize: 100,
+        lightsailURL: 'https://ecm-inbound.able-group.de/invoice?stage=version',
+        lightsailUser: process.env.LIGHTSAIL_USER,
+        lightsailPass: process.env.LIGHTSAIL_PASS,
     };
     // Default: PROD
     default: return {
+        stage: 'prod',
+        host: 'https://able-group.d-velop.cloud',
+        repositoryID: '1a2cde3f-2913-3dc2-4a2e-e623459ac23a',
+        pageSize: 100,
+        lightsailURL: 'https://ecm-inbound.able-group.de/invoice?stage=prod',
+        lightsailUser: process.env.LIGHTSAIL_USER,
+        lightsailPass: process.env.LIGHTSAIL_PASS,
     };
     }
 }
